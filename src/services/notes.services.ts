46f7a621ff93ee7c9d5bbcb70e536b54f5c9fc6d
Ricardo Medina <rcrdmedina6@gmail.com>
2018-12-11 10:31:56 -0400

    Creando un servicio de conexión a Firebase

---
+++ src/services/notes.services.ts
@@ -0,0 +1,25 @@
+import { AngularFireDatabase } from '@angular/fire/database';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+
+export class NotesService {
+  constructor (public afDB: AngularFireDatabase) {
+
+  }
+  public getNotes () {
+    return this.afDB.list('/notes/');
+  }
+  public getNote(id) {
+    return this.afDB.object('/notes/' + id);
+  }
+  public createNote(note) {
+    return this.afDB.object('/notes/' + note.id + '/').set(note);
+  }
+  public editNote(note) {
+    return this.afDB.object('/notes/' + note.id + '/').set(note);
+  }
+  public deleteNote(id) {
+    return this.afDB.object('/notes/' + id).remove();
+  }
+}
